526750a844aef314eb1cbee712e9a1c94ec70929
Denis-alt254 <deniskipkurui2024@gmail.com>
2026-01-16 15:22:49 +0300

    Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Formats.Asn1;
+using System.Net.Http.Headers;
+
+class Program
+{
+    static void CalculateArea()
+    {
+        Console.WriteLine("Enter the length = ");
+        double length = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the width = ");
+        double width = Convert.ToDouble(Console.ReadLine());
+
+        double area = Convert.ToDouble(length * width);
+
+        Console.WriteLine("Area = " + area + " cm^2");
+    }
+
+    static void Main(string [] args)
+    {
+        CalculateArea();
+    }
+}
