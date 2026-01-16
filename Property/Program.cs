5ffd8eeae3d8ab762e01a989cefa5ec390fb2cf5
Denis-alt254 <deniskipkurui2024@gmail.com>
2026-01-16 16:57:08 +0300

    Properties

---
+++ Property/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace person
+{
+    class Person
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            Person myObj = new Person();
+            myObj.Name = "Denis";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
