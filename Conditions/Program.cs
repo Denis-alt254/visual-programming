9b082ef878e312c4b62cf075b85ac985ccb901db
Denis-alt254 <deniskipkurui2024@gmail.com>
2026-01-16 15:01:35 +0300

    conditions and looping through an array

---
+++ Conditions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string [] args)
+    {
+        int [] numbers = {1, 2, 3, 4, 5};
+
+        Console.WriteLine("foreach loop");
+
+        foreach(int i in numbers)
+        {
+            Console.WriteLine(i);
+        }
+
+        Console.WriteLine("For loop");
+
+        for(int j = 0; j< numbers.GetLength(0); j++)
+        {
+            Console.WriteLine(numbers[j]);
+        }
+    }
+}
