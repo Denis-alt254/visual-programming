2e741754ec8c6cb638275bb43d192e31eb20d4ed
Denis-alt254 <deniskipkurui2024@gmail.com>
2026-01-27 15:34:10 +0300

    Testing mysql connection

---
+++ Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "server=localhost;" +
+                                  "user=root;" +
+                                  "password=Deno8758@!;" +
+                                  "database=testdb;";
+
+        MySqlConnection connection = new MySqlConnection(connectionString);
+
+        try
+        {
+            connection.Open();
+            Console.WriteLine("Connected to MySQL successfully!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        finally
+        {
+            connection.Close();
+        }
+    }
+}
