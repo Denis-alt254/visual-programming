a44a9397704db3a08901f9d7e18fdfeadaf12586
Denis-alt254 <deniskipkurui2024@gmail.com>
2026-03-24 14:19:49 +0300

    polymorphism

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,27 @@
+﻿class Animal
+{
+    public virtual void Sound()
+    {
+        Console.WriteLine("The animal make sound.");
+    }
+}
+
+class Dog: Animal
+{
+    public override void Sound()
+    {
+        Console.WriteLine("Dog burks");
+    }
+}
+
+class Engine
+{
+    static void Main(string [] args)
+    {
+        Animal myanimal = new Animal();
+        Animal dog = new Dog();
+
+        myanimal.Sound();
+        dog.Sound();
+    }
+}
